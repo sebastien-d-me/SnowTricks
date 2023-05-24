d26bf3e8e483692fd6a171dc22d20d286a57b5aa
Sébastien D <sebastien.delahaye.contact@gmail.com>
2023-05-24 22:46:09 +0200

    🚧 Continuation of the creation
    Continuation of the creation

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -4,6 +4,7 @@ const currentURL = window.location.pathname;
 
 /** Home page **/
 if (currentURL === "/") {
+    // Manage the arrow
     const tricksArrow = document.querySelector(".tricks__arrow");
     let tricksArrowDisplayed = false;
     const btnTricks = document.querySelector(".btn__tricks");
@@ -22,6 +23,24 @@ if (currentURL === "/") {
             tricksArrow.classList.remove("show");
         } else if (tricksArrowDisplayed === true && window.scrollY > 200) {
             tricksArrow.classList.add("show");
+        }
+    });
+
+    // Manage the button
+    const tricks = document.querySelectorAll(".tricks__item")
+    const tricksNumber = tricks.length;
+    const tricksButton = document.querySelector(".btn__tricks");
+    let tricksLoaded = 15;
+
+    tricksButton.addEventListener("click", function () {
+        for (let tricksIndex = tricksLoaded; tricksIndex < tricksLoaded + 5 && tricksIndex < tricksNumber; tricksIndex++) {
+            tricks[tricksIndex].classList.add("show");
+        }
+
+        tricksLoaded += 5;
+
+        if (tricksLoaded >= tricksNumber) {
+            tricksButton.classList.add("hide");
         }
     });
 }
