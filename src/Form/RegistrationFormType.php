f42acb734cd6fabb13726a7e5eed8be3a55f6c40
Sébastien D <sebastien.delahaye.contact@gmail.com>
2023-05-31 20:13:00 +0200

    🚧 Continuation of the creation
    Continuation of the creation

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,7 +2,6 @@
 
 namespace App\Form;
 
-
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -16,48 +15,52 @@ use App\Entity\LoginCredentials;
 
 class RegistrationFormType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options): void {
-        $builder
-            ->add("username", TextType::class, [
-                "error_bubbling" => true,
-                "constraints" => [
-                    new NotBlank([
-                        "message" => "Veuillez écrire votre nom d'utilisateur.",
-                    ]),
-                    new Length([
-                        "min" => 2,
-                        "minMessage" => "Votre nom d'utilisateur doit contenir au moins {{ limit }} caractères.",
-                        "max" => 4096,
-                    ])
-                ],
-            ])
-            ->add("email", EmailType::class, [
-                "error_bubbling" => true,
-                "constraints" => [
-                    new NotBlank([
-                        "message" => "Veuillez écrire votre adresse email.",
-                    ])
-                ]
-            ])
-            ->add("password", PasswordType::class, [
-                "error_bubbling" => true,
-                "mapped" => false,
-                "constraints" => [
-                    new NotBlank([
-                        "message" => "Veuillez écrire votre mot de passe.",
-                    ]),
-                    new Length([
-                        "min" => 6,
-                        "minMessage" => "Votre mot de passe doit contenir au moins {{ limit }} caractères.",
-                        "max" => 4096,
-                    ])
-                ],
-            ])
-        ;
+        $builder->add("username", TextType::class, [
+            "constraints" => [
+                new Length([
+                    "max" => 4096,
+                    "min" => 2,
+                    "minMessage" => "Votre nom d'utilisateur doit contenir au moins {{ limit }} caractères."
+                ]),
+                new NotBlank([
+                    "message" => "Veuillez écrire votre nom d'utilisateur."
+                ])
+            ],
+            "error_bubbling" => true
+        ]);
+
+        $builder->add("email", EmailType::class, [
+            "constraints" => [
+                new NotBlank([
+                    "message" => "Veuillez écrire votre adresse email.",
+                ])
+            ],
+            "error_bubbling" => true
+        ]);
+
+        $builder->add("password", PasswordType::class, [
+            "constraints" => [
+                new Length([
+                    "max" => 4096,
+                    "min" => 6,
+                    "minMessage" => "Votre mot de passe doit contenir au moins {{ limit }} caractères.",
+                        
+                ]),
+                new NotBlank([
+                    "message" => "Veuillez écrire votre mot de passe.",
+                ])
+            ],
+            "error_bubbling" => true,
+            "mapped" => false
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void {
         $resolver->setDefaults([
-            "data_class" => LoginCredentials::class,
+            "attr" => [
+                "class" => "form__block",
+            ],
+            "data_class" => LoginCredentials::class
         ]);
     }
 }
